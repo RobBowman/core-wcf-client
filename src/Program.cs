317c19d7d8ae534be672ffdf62ef82aaa2eaf41a
Rob Bowman <rob.bowman@leeds.gov.uk>
2023-01-12 10:31:47 +0000

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿using ServiceReference;
+// Instantiate the Service wrapper specifying the binding and optionally the Endpoint URL. The BasicHttpBinding could be used instead.
+
+var client  = new NumberConversionSoapTypeClient(NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap,
+            "https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+
+Console.WriteLine("Going to call the web service...");
+var response = await client.NumberToWordsAsync(46);
+
+Console.WriteLine($"The response is: {response.Body.NumberToWordsResult}");;
