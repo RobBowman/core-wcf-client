78bb9b9a312bd712c2e11fc8b57271febf34e487
Rob Bowman <rob.bowman@leeds.gov.uk>
2023-02-08 15:09:01 +0000

    wip

--- src/HttpTrigger1.cs
+++ src/HttpTrigger1.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using ServiceReference;
 
 namespace Company.Function
 {
@@ -15,14 +16,22 @@ namespace Company.Function
         }
 
         [Function("HttpTrigger1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            _logger.LogInformation("Going to call WCF service...");
+
+            var client  = new NumberConversionSoapTypeClient(NumberConversionSoapTypeClient.EndpointConfiguration.NumberConversionSoap,
+            "https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+
+            var result = await client.NumberToWordsAsync(46);
+
+            response.WriteString($"Response={result.ToString()}");
+
 
             return response;
         }
